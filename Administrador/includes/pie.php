74c7df96e5944dcdbd473a0513fd525e5046723f
Claudio <Claudio@MacPlace>
2019-02-12 12:54:57 +0100

    Empezamos!!

--- Administrador/includes/pie.php
+++
@@ -1,5 +0,0 @@
- <footer class="pie_admin">
-          <div class="container-fluid text-center">
-              Revista Alimentaria <?php echo date("Y");?> &reg; 
-          </div>
-      </footer>
--- funciones.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function cambiarFormato($string) {
-    $fecha=DateTime::createFromFormat('d/m/Y H:i', $string);
-    $fecha_final= $fecha->format('Y-m-d h:i');
-    return $fecha_final;
-}
-?>
